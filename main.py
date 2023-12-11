c82512249fd25060243349a1c81191c922d78d30
1ArtAlex <91719015+1ArtAlex@users.noreply.github.com>
2023-12-11 10:17:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 bot = Bot('6922972034:AAHRY5dZQ7w4pA3P0ZIn80rs9JqE3oN4FrU')
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=("start"))
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
     markup.add(types.KeyboardButton('Открыть', web_app=WebAppInfo(url='https://delightful-chebakia-2e4cca.netlify.app')))
-    await message.answer('Автомбили в наличии 🚘:', reply_markup=markup)
+    await message.answer('Автомобили в наличии 🚘:', reply_markup=markup)
 
-@dp.message_handler(content_types=['wed_app_data'])
-async def wed_app(message: types.Message):
-    await message.answer(message.web_app_data.data)
+@dp.message_handler(content_types=['web_app'])
+async def web_app_handler(message: types.Message):
+    # Обработка данных от веб-приложения
+    web_app_data = message.web_app_data
+    await message.answer(f"Приняты данные от веб-приложения: {web_app_data}")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
